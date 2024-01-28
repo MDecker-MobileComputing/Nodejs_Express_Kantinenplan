9f27498c10681fd7e6c52572c9a39ce0c3251d83
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-01-28 11:55:18 +0100

    Verzeichnis public/ dazu

--- README.md
+++ README.md
@@ -72,3 +72,15 @@ Gerichte für den Tag zu definieren.
 
 <br>
 
+----
+
+## License ##
+
+<br>
+
+See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License)
+for the files in this repository.
+
+<br>
+
+
--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express();
 
 const PORT_NUMMER = 8080;
 
+/* "Datenbank" für Gerichte */
+const datumZuGerichteArray = {
+
+    "1900-01-01": [ "Spaghetti mit Tomatensoße", "Salamipizza" ],
+    "1900-01-31": [ "Risi Bisi", "Spinat mit Salzkartoffeln" ],
+};
+
+
+// statische Dateien (z.B. index.html) aus Unterordner "public/" bereitstellen
+app.use( express.static("public") );
 
 
 app.listen( PORT_NUMMER,
