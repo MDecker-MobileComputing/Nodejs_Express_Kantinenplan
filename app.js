4f384e65b803bfdaee08949d528cc195bd4a8a12
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-01-28 13:53:53 +0100

    HTTP-POST-Request: Weitere Gerichte werden jetzt an Array angehaengt

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const datumZuGerichteMap = {
     "1900-01-31": [ "Risi Bisi", "Spinat mit Salzkartoffeln" ],
 };
 
+// Damit Body von HTTP-POST-Request als JSON interpretiert wird
+app.use( express.json() );
+
+// statische Dateien (z.B. index.html) aus Unterordner "public/" bereitstellen
+app.use( express.static("public") );
+
 
 /*
  * Endpunkt für HTTP-GET-Request zur Abfrage der Gerichte für einen Tag.
@@ -43,27 +49,37 @@ app.get("/kantinenplan/abfrage/:datum", (req, res) => {
     }
 });
 
-// Damit Body von HTTP-POST-Request als JSON interpretiert wird
-app.use( express.json() );
 
+/*
+ * Endpunkt für HTTP-POST-Request zum Einplanen eines Gerichts für einen Tag.
+ */
 app.post("/kantinenplan/einplanen/", (req, res) => {
 
-    const datum = req.body.datum;
+    const datum   = req.body.datum;
     const gericht = req.body.gericht;
 
-    datumZuGerichteMap[datum] = [ gericht ];
+    let nachricht = "";
+    const gerichte = datumZuGerichteMap[datum];
+    if (!gerichte) {
+
+        datumZuGerichteMap[datum] = [ gericht ];
+        nachricht = "Erstes Gericht für Tag eingeplant";
+
+    } else {
+
+        gerichte.push(gericht);
+        nachricht = "Weiteres Gericht für Tag eingeplant.";
+    }
 
     res.status(201)
        .json({
             "datum"    : datum,
             "erfolg"   : true,
-            "nachricht": "Gericht wurde eingeplant"
+            "nachricht": nachricht
         });
 });
 
 
-// statische Dateien (z.B. index.html) aus Unterordner "public/" bereitstellen
-app.use( express.static("public") );
 
 // Web-Server starten
 app.listen( PORT_NUMMER,
